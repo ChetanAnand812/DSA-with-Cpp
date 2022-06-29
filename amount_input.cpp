2321a59eb0b08b37c03c6d30da123b9c933ba18e
ChetanAnand812 <chetananand812@gmail.com>
2022-06-29 21:20:56 +0530

    initial

---
+++ amount_input.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int amt1, amt2;
+    cout<<"Enter amt1: \n";
+    cin>>amt1;
+    cout<<"Enter amt2: \n";
+    cin>>amt2;
+
+    int add = amt1 + amt2;
+    cout<<"Addtion of amt1 and amt2 is "<<add; 
+
+}
---
+++ odd_even.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n;
+    cout<<"Enter n: \n";
+    cin>>n;
+
+    if (n%2 == 0){
+        cout<<"Number is Even\n";
+    }
+    else {
+        cout<<"Number is Odd";
+    }
+    
+}
